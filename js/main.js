777ee08d22893b73a4705cc3655dae396cbc50fe
Muhammad Harun <muhharuntahir@gmail.com>
2022-11-21 13:34:31 +0800

    Fixed header menu button

--- js/main.js
+++ js/main.js
@@ -34,3 +34,12 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
 }
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        });
+    });
+});
+
